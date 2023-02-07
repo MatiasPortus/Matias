3f70b53e9f973163fdece550b729b5d13e012f06
Matias <alumno.532989@ies-azarquiel.es>
2023-02-07 09:18:03 +0100

    WishList Terminado

--- src/App/WishList/WishItem.jsx
+++ src/App/WishList/WishItem.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 function WishItem({
-  text, id, done, onDoneChange,
+  id, done, text, onDoneChange,
 }) {
+  const [age, setAge] = useState(0);
+  useEffect(() => {
+    let ageInterval;
+    if (done) {
+      // si el deseo es completado dejo de contar y limpio el intervalo
+      setAge(0);
+      clearInterval(ageInterval);
+    } else {
+      // si el deseo no esta completado creo un intervalo que incrementa en 1
+      // la edad del deseo cada 1000 milisegundos
+      ageInterval = setInterval(() => setAge((a) => a + 1), 1000);
+    }
+    // la funcion que devuelve return se ejercita cuando se desmonta el componente
+    return () => clearInterval(ageInterval);
+  }, [done]);
   return (
     <li
-      className={`wish-list__item
-      ${done ? 'wish-list__item--done' : ''}`}
+      className={classNames('wish-list__item', {
+        'wish-list__item--done': done,
+        'wish-list__item--warn': age >= 5 && age < 10,
+        'wish-list__item--danger': age >= 10,
+      })}
     >
       <input
         type="checkbox"
